a07bedf91d9d37781e1407456828860dd5605edd
NowshinBushra <nowshin.bushra399@gmail.com>
2025-09-06 20:42:01 +0600

    email activation

--- blood_bank/settings.py
+++ blood_bank/settings.py
@@ -175,6 +175,9 @@ SIMPLE_JWT = {
 }
 
 DJOSER = {
+    'PASSWORD_RESET_CONFIRM_URL': 'password/reset/confirm/{uid}/{token}',
+    'ACTIVATION_URL': 'activate/{uid}/{token}',
+    'SEND_ACTIVATION_EMAIL': True,
     "PERMISSIONS": {
         "user_create": ["rest_framework.permissions.IsAdminUser"],  
         "user_list":   ["rest_framework.permissions.IsAdminUser"],  
@@ -196,3 +199,12 @@ SWAGGER_SETTINGS = {
       }
    }
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
+# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
