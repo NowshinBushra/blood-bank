940bc9fae7c11aab1ec48c9e12533129a406751b
NowshinBushra <nowshin.bushra399@gmail.com>
2026-06-21 16:24:07 +0600

    fixed pass confirmation

--- users/serializers.py
+++ users/serializers.py
@@ -6,6 +6,7 @@ from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer,
 class UserCreateSerializer(BaseUserCreateSerializer):
     user_image = serializers.ImageField(required=False, allow_null=True)
     blood_group = serializers.CharField(required=False, allow_null=True, allow_blank=True)
+    re_password = serializers.CharField(write_only=True)
 
     class Meta(BaseUserCreateSerializer.Meta):
         fields = ['id', 'email', 'password', 're_password', 'name', 'blood_group', 'user_image', 'address',
